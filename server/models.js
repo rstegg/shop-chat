02e8962ff57c52160b4d7fe2f54df3e96f62d888
Ryan <stegartie@gmail.com>
2017-05-07 11:29:29 -0700

    update models

--- server/models.js
+++ server/models.js
@@ -1,8 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   return {
-    Article: require('./model/article')(sequelize, DataTypes),
     User: require('./model/user')(sequelize, DataTypes),
-    Post: require('./model/post')(sequelize, DataTypes),
     Shop: require('./model/shop')(sequelize, DataTypes),
     Payment: require('./model/payment')(sequelize, DataTypes),
   }

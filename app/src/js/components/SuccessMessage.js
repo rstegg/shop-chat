f28828905569e034f1a9d321db8aa78707c8aab1
Ryan <stegartie@gmail.com>
2017-05-09 21:55:25 -0700

    FIX SUCCESSMESSAGE BUG. GOD IM STUPID

--- app/src/js/components/SuccessMessage.js
+++ app/src/js/components/SuccessMessage.js
@@ -1,35 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
 import { resetSignup } from '../redux/actions/signup'
 import { createProduct } from '../redux/actions/products'
 
-class SuccessMessage extends Component {
-  componentWillUpdate() {
-    if(this.props.visible) {
-      if(this.props.product.isSaved) {
-        this.props.createProduct(this.props.product, this.props.user)
-      }
-    }
-  }
-  render() {
-    const { visible, resetSignup } = this.props
-    return (
-      <Modal open={visible} onClose={resetSignup} basic size='small'>
-        <Header icon='mail' content='Success!' />
-      <Modal.Content>
-        <h3>Check your email for verification.</h3>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color='green' onClick={resetSignup} inverted>
-          <Icon name='checkmark' />
-        </Button>
-      </Modal.Actions>
-    </Modal>
-    )
-  }
-}
+const SuccessMessage = ({visible, resetSignup}) =>
+  <Modal open={visible} onClose={resetSignup} basic size='small'>
+    <Header icon='mail' content='Success!' />
+  <Modal.Content>
+    <h3>Check your email for verification.</h3>
+  </Modal.Content>
+  <Modal.Actions>
+    <Button color='green' onClick={resetSignup} inverted>
+      <Icon name='checkmark' />
+    </Button>
+  </Modal.Actions>
+</Modal>
 
 const mapStateToProps = ({user}) =>
 ({

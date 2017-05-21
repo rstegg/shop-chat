ff1d3d885e186dc2767af21ef8da3c1b32a50a52
Ryan <stegartie@gmail.com>
2017-05-20 19:42:12 -0700

    Minor tweaks to grid styling [desktop]

--- app/src/js/components/layouts/Grid/DesktopGrid.js
+++ app/src/js/components/layouts/Grid/DesktopGrid.js
@@ -3,17 +3,19 @@ import { Grid, Segment } from 'semantic-ui-react'
 
 export default ({Image, Canopy, ChatBox, Header, SubHeader, Gutter, GutterRight}) =>
   <Grid celled='internally' className='main-container'>
-    <Grid.Column width={3}>
-      <Segment>{Image}</Segment>
-      <Segment>{Header}</Segment>
-      <Segment>{SubHeader}</Segment>
-      <Segment>{Gutter}</Segment>
-      <Segment>{GutterRight}</Segment>
+    <Grid.Column width={3} stretched>
+      <Segment basic>
+        <Segment>{Image}</Segment>
+        <Segment>{Header}</Segment>
+        <Segment>{SubHeader}</Segment>
+        <Segment>{Gutter}</Segment>
+        <Segment>{GutterRight}</Segment>
+      </Segment>
     </Grid.Column>
     <Grid.Column width={8} stretched>
-      {Canopy}
+      <Segment basic>{Canopy}</Segment>
     </Grid.Column>
-    <Grid.Column width={5}>
+    <Grid.Column width={5} stretched>
       {ChatBox}
     </Grid.Column>
   </Grid>

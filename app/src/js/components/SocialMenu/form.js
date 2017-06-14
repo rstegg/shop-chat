07436bbe37137a66d0ed67d93fe84bded6b140c6
Ryan <stegartie@gmail.com>
2017-06-14 12:15:59 -0700

    Add validators to social menu

--- app/src/js/components/SocialMenu/form.js
+++ app/src/js/components/SocialMenu/form.js
@@ -6,14 +6,17 @@ import { Form } from 'semantic-ui-react'
 import InputField from 'elements/Input/InputField'
 import AreaField from 'elements/Input/AreaField'
 
+import { validate } from './validators'
+
 const SocialMenuForm = ({handleSubmit}) =>
   <Form onSubmit={handleSubmit}>
     <Field component={InputField} name='name' label='Name' placeholder='Name' />
     <Field component={InputField} type='email' name='email' label='To' placeholder='Email' />
-    <Field component={AreaField} name='message' label='Custom Message' placeholder='Body' />
+    <Field component={AreaField} name='message' label='Custom Message' placeholder='Optional' />
     <Form.Button type='submit' primary>Send</Form.Button>
   </Form>
 
 export default reduxForm({
-  form: 'productInvite'
+  form: 'shareInvite',
+  validate
 })(SocialMenuForm)

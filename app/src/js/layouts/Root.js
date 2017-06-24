28d48f541f05dc2c7a2d7984b81a5f2eb40482b9
Ryan <stegartie@gmail.com>
2017-06-23 18:36:12 -0700

    Remove draggable and html5 drag n drop context

--- app/src/js/layouts/Root.js
+++ app/src/js/layouts/Root.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import { DragDropContext } from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
-
 import Header from 'components/Header'
 import ProductSidebar from 'components/Product/Sidebar'
 import OverlaySwitch from './Overlay'
@@ -31,4 +28,4 @@ const RootLayout = ({children}) =>
       <OverlaySwitch />
     </div>
 
-export default DragDropContext(HTML5Backend)(RootLayout)
+export default RootLayout
